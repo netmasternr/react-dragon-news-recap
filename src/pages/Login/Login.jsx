3b926e38f42e31c06d32f5acb544501cd277c9fa
netmasternr <netmasternr@gmail.com>
2024-04-07 16:55:49 +0600

    first commit

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar/>
+            <h2 className="text-3">this is login</h2>
+        </div>
+    );
+};
+
+export default Login;
